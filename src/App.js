516dd86276ac5e557ba31bb82334a4990bbb6ed1
duykhanhtrandev <109634649+duykhanhtrandev@users.noreply.github.com>
2023-03-29 22:52:15 +0700

    jsx and App components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
-function App() {
-  return <h1>Hello World!</h1>;
-}
+import './style.css';
 
+function App() {
+  return (
+    <header className="header">
+      <div className="logo">
+        <img src="logo.png" height="68" width="68" alt="Today I Learned Logo" />
+        <h1>Today I Learned</h1>
+      </div>
+
+      <button className="btn btn-large btn-open">Share a fact</button>
+    </header>
+  );
+}
 export default App;
