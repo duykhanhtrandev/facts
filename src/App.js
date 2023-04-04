920d05d20e73fe1491c54cee7502f9bd86d79104
duykhanhtrandev <109634649+duykhanhtrandev@users.noreply.github.com>
2023-04-04 18:21:56 +0700

    creating a loader component

--- src/App.js
+++ src/App.js
@@ -53,11 +53,23 @@ import './style.css';
 function App() {
   const [showForm, setShowForm] = useState(false);
   const [facts, setFacts] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(function () {
     async function getFacts() {
-      const { data: facts, error } = await supabase.from('facts').select('*');
-      setFacts(facts);
+      setIsLoading(true);
+      const { data: facts, error } = await supabase
+        .from('facts')
+        .select('*')
+        .order('votesInteresting', { ascending: false })
+        .limit(1000);
+
+      console.log(facts);
+      console.log(error);
+
+      if (!error) setFacts(facts);
+      else alert('There was a problem getting data');
+      setIsLoading(false);
     }
     getFacts();
   }, []);
@@ -71,10 +83,15 @@ function App() {
 
       <main className="main">
         <CategoryFilter />
-        <FactList facts={facts} />
+
+        {isLoading ? <Loader /> : <FactList facts={facts} />}
       </main>
     </>
   );
+}
+
+function Loader() {
+  return <p className="message">Loading...</p>;
 }
 
 function Header({ showForm, setShowForm }) {
