3bde3e0d0e9846c4ce2f7273f9a8bdbc9d8b06ee
Duy Khánh <109634649+duykhanhtrandev@users.noreply.github.com>
2023-04-20 09:32:27 +0700

    finishing touches

--- src/App.js
+++ src/App.js
@@ -2,54 +2,6 @@ import { useEffect, useState } from "react";
 import supabase from "./supabase";
 import "./style.css";
 
-// const initialFacts = [
-//   {
-//     id: 1,
-//     text: 'React is being developed by Meta (formerly facebook)',
-//     source: 'https://opensource.fb.com/',
-//     category: 'technology',
-//     votesInteresting: 24,
-//     votesMindblowing: 9,
-//     votesFalse: 4,
-//     createdIn: 2021,
-//   },
-//   {
-//     id: 2,
-//     text: 'Millennial dads spend 3 times as much time with their kids than their fathers spent with them. In 1982, 43% of fathers had never changed a diaper. Today, that number is down to 3%',
-//     source:
-//       'https://www.mother.ly/parenting/millennial-dads-spend-more-time-with-their-kids',
-//     category: 'society',
-//     votesInteresting: 11,
-//     votesMindblowing: 2,
-//     votesFalse: 0,
-//     createdIn: 2019,
-//   },
-//   {
-//     id: 3,
-//     text: 'Lisbon is the capital of Portugal',
-//     source: 'https://en.wikipedia.org/wiki/Lisbon',
-//     category: 'society',
-//     votesInteresting: 8,
-//     votesMindblowing: 3,
-//     votesFalse: 1,
-//     createdIn: 2015,
-//   },
-// ];
-
-// function Counter() {
-//   const [count, setCount] = useState(0);
-//   console.log(count);
-
-//   return (
-//     <div>
-//       <span style={{ fontSize: '40px' }}>{count}</span>
-//       <button className="btn btn-large" onClick={() => setCount((c) => c + 1)}>
-//         +1
-//       </button>
-//     </div>
-//   );
-// }
-
 function App() {
   const [showForm, setShowForm] = useState(false);
   const [facts, setFacts] = useState([]);
@@ -158,18 +110,6 @@ function NewFactForm({ setFacts, setShowForm }) {
 
     // 2. Check if data is valid. If so, create a new fact
     if (text && isValidHttpUrl(source) && category && textLength <= 200) {
-      // 3. Create a new fact object
-      // const newFact = {
-      //   id: Math.round(Math.random() * 10000000),
-      //   text,
-      //   source,
-      //   category,
-      //   votesInteresting: 0,
-      //   votesMindblowing: 0,
-      //   votesFalse: 0,
-      //   createdIn: new Date().getFullYear(),
-      // };
-
       // .3 Upload fact to Supabase and receive the new fact object
       setIsUploading(true);
       const { data: newFact, error } = await supabase
@@ -278,6 +218,8 @@ function FactList({ facts, setFacts }) {
 
 function Fact({ fact, setFacts }) {
   const [isUploading, setIsUploading] = useState(false);
+  const isDisputed =
+    fact.votesInteresting + fact.votesMindblowing < fact.votesFalse;
 
   async function handleVote(columnName) {
     setIsUploading(true);
@@ -298,6 +240,7 @@ function Fact({ fact, setFacts }) {
   return (
     <li className="fact">
       <p>
+        {isDisputed ? <span className="disputed">[⛔ DISPUTED]</span> : null}
         {fact.text}
         <a
           className="source"
